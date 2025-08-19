c842fff8f546869c4097eb229f344232a3384f99
Abhay627 <gautamabhay3009@gmail.com>
2025-08-20 01:36:24 +0530

    Build initial user interface

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,18 @@
+import {Link} from "react-router";
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <Link to="/">
+                <p className="text-2xl font-bold text-gradient">
+                    ResumeXpert
+                </p>
+            </Link>
+            <Link to="/about" className="primary-button w-fit">
+                Upload Resume
+            </Link>
+        </nav>
+    );
+};
+
+export default Navbar;
